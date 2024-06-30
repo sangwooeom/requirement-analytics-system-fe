139b5c0700b68ac24eab927845792208aee2d16d
sangwooeom <cdm19720@gmail.com>
2024-07-01 06:22:38 +0900

    변경

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,17 +1,25 @@
 import cn from 'classnames';
 import styles from './Login.module.scss';
+import { Container } from '../../components';
+import { Form, Button } from 'react-bootstrap';
 
 export default function Login() {
     return (
-        <div className={cn(styles.container)}>
+        <Container>
             <div className={cn(styles.loginForm)}>
-                <h1 className={cn(styles.title)}>로그인</h1>
-                <div className={cn(styles.inputSection)}>
-                    <input placeholder='아이디'/>
-                    <input type='password' placeholder='비밀번호'/>
+                <div className={cn(styles.header)}>
+                    <h1>요구사항 분석 시스템</h1>
                 </div>
-                <button className={cn(styles.loginBtn)}>로그인</button>
+                <Form.Group>
+                    <Form.Label>아이디</Form.Label>
+                    <Form.Control />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>비밀번호</Form.Label>
+                    <Form.Control />
+                </Form.Group>
+                <Button variant='outline-secondary'>로그인</Button>
             </div>
-        </div>
+        </Container>
     )
 }
